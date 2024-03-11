fa3b08496c75ca95630084318927fe757cc4c2d8
ShinSeongChang <ssin011123@gmail.com>
2024-03-11 22:02:59 +0900

    [Test] : 맵 생성 후, 목표지점 타일 및 해당 지점까지 경로 확인하는 코드 테스트중

--- Unrailed/Assets/01.Shin/03.Map/Scripts/MapBoard.cs
+++ Unrailed/Assets/01.Shin/03.Map/Scripts/MapBoard.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapBoard : MonoBehaviour
 {
@@ -11,14 +13,18 @@ public class MapBoard : MonoBehaviour
 
     Vector2Int size = default;
 
+    Queue<MapTile> searchFrontier = new Queue<MapTile>();
+
+    // 지형 생성 메서드
     public void Initialize(Vector2Int size)
     {
-        mapTiles = new MapTile[size.x * size * y];
 
         this.size = size;
         ground.localScale = new Vector3(size.x, size.y, 1);
             
         Vector2 offset = new Vector2(size.x - 1 * 0.5f, size.y - 1 * 0.5f);
+
+        mapTiles = new MapTile[size.x * size.y];
 
         for(int i = 0, y = 0; y < size.y; y++)
         {
@@ -28,9 +34,76 @@ public class MapBoard : MonoBehaviour
                 mapTiles[i] = tile;
 
                 tile.transform.SetParent(transform, false);
-                tile.transform.localRotation = Quaternion.Euler(90, 0, 0);
                 tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
+
+
+                // { 맵 생성시 이웃들 정의해주기
+                if(x > 0)
+                {
+                    MapTile.MakeEastWestNeighbors(tile, mapTiles[i - 1]);
+                }
+
+                if(y > 0)
+                {
+                    MapTile.MakeNorthSouthNeighbors(tile, mapTiles[i - size.x]);
+                }
+                // } 맵 생성시 이웃들 정의해주기
+
+                tile.isAlternative = (x & 1) == 0;
+
+                if((y & 1) == 0)
+                {
+                    tile.isAlternative = !tile.isAlternative;
+                }
             }
+        }
+
+        FindPaths();    
+    }
+
+    void FindPaths()
+    {
+        // 모든 타일 경로 초기화
+        foreach(MapTile tile in mapTiles)
+        {
+            tile.ClearPath();
+        }
+
+        // 목표가 될 타일 지정
+        mapTiles[mapTiles.Length / 2].BecomDestination();
+        searchFrontier.Enqueue(mapTiles[mapTiles.Length / 2]);
+
+
+        // 03.11 시점에서 봤을 때 경로 찾는 것은 목표지점에서 부터 역순으로 자신의 이웃들에게 경로를 부여하는 듯 함
+        while (searchFrontier.Count > 0)
+        {
+            MapTile tile = searchFrontier.Dequeue();
+
+            if(tile != null)
+            {                
+                // if문이 서로 반대의 순서, 지그재그 방식으로 길찾기 진행
+                if(tile.isAlternative)
+                {
+                    searchFrontier.Enqueue(tile.GrowPathNorth());
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                }
+                else
+                {
+                    searchFrontier.Enqueue(tile.GrowPathWest());
+                    searchFrontier.Enqueue(tile.GrowPathEast());
+                    searchFrontier.Enqueue(tile.GrowPathSouth());
+                    searchFrontier.Enqueue(tile.GrowPathNorth());
+                }
+
+            }
+
+        }
+
+        foreach (MapTile tile in mapTiles)
+        {
+            tile.ShowPath();
         }
     }
 
--- Unrailed/Assets/01.Shin/03.Map/Scripts/MapTile.cs
+++ Unrailed/Assets/01.Shin/03.Map/Scripts/MapTile.cs
@@ -6,5 +6,90 @@ public class MapTile : MonoBehaviour
 {
     [SerializeField] Transform arrow = default;
 
+    // 자신의 이웃 타일 및 다음경로 담아두기
+    MapTile east, west, north, south, nextOnPath;
+    int distance;
 
+    // bool 값은 0과 1로도 표현 가능
+    // 즉 무한대의 거리가 아니라면 경로가 존재한다 = true
+    public bool isPath => distance != int.MaxValue;
+    public bool isAlternative { get; set; }
+
+    static Quaternion
+        norhRotation = Quaternion.Euler(90f, 0f, 0f),
+        southRotation = Quaternion.Euler(90f, 180f, 0f),
+        eastRotation = Quaternion.Euler(90f, 90f, 0f),
+        westRotation = Quaternion.Euler(90f, 270f, 0f);
+
+    MapTile GrowPathTo(MapTile neighbor)
+    {
+        Debug.Assert(isPath, "No Neighbor!");
+
+        // 경로가 없거나, 전달받은 이웃이 없거나, 이웃의 경로가 없다면 null 반환
+        if (!isPath || neighbor == null || neighbor.isPath) { return null; }
+
+        // 이웃의 경로 갱신(이동할 수 있는 칸이 있냐 없냐)
+        neighbor.distance = distance + 1;
+
+        // 이웃의 경로는 나 자신
+        neighbor.nextOnPath = this;
+
+        return neighbor;
+    }
+
+    #region PublicResponsiblity
+
+    // { 서로 대칭관계의 방향 이웃 정의해주기
+    public static void MakeEastWestNeighbors(MapTile east, MapTile west)
+    {
+        Debug.Assert(west.east == null && east.west == null, "Redefined Neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(MapTile north, MapTile south)
+    {
+        Debug.Assert(north.south == null && south.north == null, "Redefined Neighbors!");
+        south.north = north;
+        north.south = south;
+    }
+    // } 서로 대칭관계의 방향 이웃 정의해주기
+
+    // 경로를 찾기전 아직 대상타일이 존재하지 않는다고 정의하는 메서드 (distance == 무한대의 거리)
+    public void ClearPath()
+    {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+    
+    // 최종 목적지 정의 메서드
+    public void BecomDestination()
+    {
+        distance = 0;
+        nextOnPath = null;
+    }
+
+    public MapTile GrowPathNorth() => GrowPathTo(north);
+    public MapTile GrowPathSouth() => GrowPathTo(south);
+    public MapTile GrowPathEast() => GrowPathTo(east);
+    public MapTile GrowPathWest() => GrowPathTo(west);
+
+    // 화살표 타일이 어느방향을 바라봐야 하는지 정의해주는 메서드
+    public void ShowPath()
+    {
+        if(distance == 0)
+        {
+            arrow.gameObject.SetActive(false);
+            return;
+        }
+
+        arrow.gameObject.SetActive(true);
+        arrow.localRotation =
+            nextOnPath == north ? norhRotation :
+            nextOnPath == east ? eastRotation :
+            nextOnPath == south ? southRotation :
+            westRotation;
+    }
+
+    #endregion
 }
